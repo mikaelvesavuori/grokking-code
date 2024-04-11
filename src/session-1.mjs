66c413d3fee3a2917be105b62b90c04106261600
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2024-04-11 10:44:20 +0200

    Update

--- src/session-1.mjs
+++ src/session-1.mjs
@@ -2,6 +2,11 @@
  * It's all about primitive data types and the use of very simple things like numbers and lists.
  */
 
+const num = 1;
+const str = '';
+const list = [];
+const obj = {};
+
 const array = ['asdf', 'abc', 'xyz'];
 
 const something = array.map((value) => {
--- src/session-2.mjs
+++ src/session-2.mjs
@@ -1,3 +1,105 @@
 /**
  * Objects allow for richer, more human semantics, and functions make behaviors possible.
  */
+
+/*
+console.log('Hi!');
+let text = '';
+text += 'I ';
+text += 'am ';
+text += 'Thom!';
+console.log(text);
+console.log('Bye!');
+*/
+
+/*
+function greet(name) {
+  sayHi();
+  present(name);
+  sayBye();
+}
+
+function present(name) {
+  //const _text = ['I ', 'am ', `${name}!`].join('');
+
+  let text = '';
+
+  text += 'I ';
+  text += 'am ';
+  text += `${name}!`;
+
+  console.log(text);
+}
+
+function sayHi() {
+  console.log('Hi!');
+}
+
+function sayBye() {
+  console.log('Bye!');
+}
+
+greet('Thom');
+*/
+
+export class Greeter {
+  // Public
+  greet(name) {
+    this.sayHi();
+    this.present(name);
+    this.sayBye();
+  }
+
+  // Private
+  present(name) {
+    //const _text = ['I ', 'am ', `${name}!`].join(''); // Functional style, no mutation
+
+    let text = '';
+
+    text += 'I ';
+    text += 'am ';
+    text += `${name}!`;
+
+    console.log(text);
+  }
+
+  // Private
+  sayHi() {
+    console.log('Hi!');
+  }
+
+  // Private
+  sayBye() {
+    console.log('Bye!');
+  }
+}
+
+//const greeter = new Greeter();
+//greeter.greet('Thom');
+
+// Bad API! Poor information hiding
+//greeter.sayHi();
+//greeter.present('someone');
+//greeter.sayBye();
+
+class Character {
+  wisdom = 5;
+
+  addStat(statName) {
+    if (statName === 'wisdom') {
+      const newValue = this.wisdom > 10 ? this.wisdom + 4 : this.wisdom + 1;
+      this.wisdom = newValue;
+      console.log(this.wisdom);
+    }
+  }
+}
+
+class Game {
+  gitGud(char) {
+    char.addStat('wisdom');
+  }
+}
+
+const char = new Character();
+const game = new Game();
+game.gitGud(char);
